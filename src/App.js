f22c441555e76ee34cdca6986603bbe07bb19339
aldhairescobar <aldhair.escobar@gmail.com>
2020-09-28 13:34:09 -0500

    count words in textarea

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ function App() {
     setText(value);
   }
 
-  console.log(text);
+  function countWords(text) {
+    const wordsArr = text.trim().split(" ");
+    const filtered = wordsArr.filter((word) => word !== "");
+
+    console.log(filtered.length);
+  }
 
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
       <h4>Time Remaining:</h4>
-      <button>Start</button>
+      <button onClick={() => countWords(text)}>Start</button>
       <h1>Word Count:</h1>
     </div>
   );
