9212751d551d74c48a1ad04f2e20392b2e973dbc
aldhairescobar <aldhair.escobar@gmail.com>
2020-09-28 13:45:58 -0500

    timeRemaining State and Countdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -15,11 +16,19 @@ function App() {
     console.log(filtered.length);
   }
 
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time Remaining:</h4>
+      <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={() => countWords(text)}>Start</button>
       <h1>Word Count:</h1>
     </div>
